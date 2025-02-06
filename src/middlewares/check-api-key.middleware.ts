21ac4e9e64404bfe352e5d0db767f801d5271d66
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-02-07 01:10:12 +0800

    init auth jwt middleware

--- src/middlewares/check-api-key.middleware.ts
+++ src/middlewares/check-api-key.middleware.ts
@@ -1,17 +1,19 @@
 import { type Request, type Response, type NextFunction } from "express";
 import { SETTINGS } from "@/configs";
+import { SendHttpResponse } from "@/utils";
 import { HttpStatusCode } from "@/types";
 
-export const CheckApiKeyMiddleware = (error: any, request: Request, response: Response, next: NextFunction) => {
+export const CheckApiKeyMiddleware = (request: Request, response: Response, next: NextFunction) => {
   const header = request.header("X-API-KEY");
 
   if (!header || header !== SETTINGS.APP_SECRET_KEY) {
-    response
-      .status(HttpStatusCode.FORBIDDEN)
-      .json({
+    SendHttpResponse(
+      response,
+      {
         message: "API_KEY_MISSING",
-      })
-      .send();
+      },
+      HttpStatusCode.FORBIDDEN
+    );
 
     return;
   }
--- src/middlewares/check-auth.middleware.ts
+++ src/middlewares/check-auth.middleware.ts
@@ -1,11 +1,34 @@
 import { type Request, type Response, type NextFunction } from "express";
+import JWT from "jsonwebtoken";
+import { SendHttpResponse } from "@/utils";
+import { SETTINGS } from "@/configs";
 import { HttpStatusCode } from "@/types";
 
-export const checkAuthMiddleware = (request: Request, response: Response, next: NextFunction) => {
+export const CheckAuthMiddleware = (request: Request, response: Response, next: NextFunction) => {
   const header = request.header("Authorization");
   const token = header?.split(" ")[1];
 
   if (!header || !token) {
+    SendHttpResponse(
+      response,
+      {
+        message: "UNAUTHORIZED",
+      },
+      HttpStatusCode.UNAUTHORIZED
+    );
+
     return;
   }
+
+  JWT.verify(token, SETTINGS.APP_JWT_SECRET_KEY, (error, user) => {
+    if (error) {
+      console.log("CheckAuthMiddleware", error);
+      SendHttpResponse(response, { message: "FORBIDDEN" }, HttpStatusCode.FORBIDDEN);
+      return;
+    }
+
+    request.user = user;
+
+    next();
+  });
 };
