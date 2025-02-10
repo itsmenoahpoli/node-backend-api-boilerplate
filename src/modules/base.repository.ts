605c212a03133a173bea155e5435fe274aab5b83
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2025-02-10 15:26:21 +0800

    initial docker (working)

--- src/modules/base.repository.ts
+++ src/modules/base.repository.ts
@@ -1,7 +1,3 @@
-import { Repository, ObjectLiteral } from "typeorm";
-
-// TODO: Finish implementation
-
-export class BaseRepository<T extends ObjectLiteral> {
-	constructor(protected repository: Repository<T>) {}
+export class BaseRepository {
+	//
 }
